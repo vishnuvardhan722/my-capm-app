24f77f27df74812c22cb2f16e32532e32cc9c8ef
VENKATA VISHNU VARDHAN REDDY DUDDUKURI <vishnuvardhankavitha75@gmail.com>
2024-10-22 13:15:40 +0000

    updated some code change in view1 controller

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -17,7 +17,7 @@ sap.ui.define([
             this.getView().getModel("EmpModel").setProperty("/TableDetails", []);
             this.byId("rbg3").setSelectedIndex(-1);
 
-            // updating some code in view1 contoller file
+            // Today updating some code in view1 contoller file
 
         },
 
